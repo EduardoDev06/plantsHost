7e2dff9b50ededcfb558b6b3dbda4eb34cb2d699
EduardoDev06 <wolf1200123@gmail.com>
2022-11-16 17:46:54 -0500

    Set up Project

--- app/src/main/java/com/bootcampnttdata6/plantshost/features/main/createPlant/CreatePlantFragment.kt
+++ app/src/main/java/com/bootcampnttdata6/plantshost/features/main/createPlant/CreatePlantFragment.kt
@@ -1,10 +1,11 @@
-package com.bootcampnttdata6.plantshost
+package com.bootcampnttdata6.plantshost.features.main.createPlant
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bootcampnttdata6.plantshost.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
