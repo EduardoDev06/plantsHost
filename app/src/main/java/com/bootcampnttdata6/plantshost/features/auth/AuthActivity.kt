d7ce25de61bbd2859426ebf58a3521b66bacf20f
EduardoDev06 <wolf1200123@gmail.com>
2022-11-22 15:45:39 -0500

    Splash Screen two step. -Mipmap Icon app was added -Auth Navigation Set Up configured

--- app/src/main/java/com/bootcampnttdata6/plantshost/features/auth/AuthActivity.kt
+++ app/src/main/java/com/bootcampnttdata6/plantshost/features/auth/AuthActivity.kt
@@ -1,15 +1,13 @@
 package com.bootcampnttdata6.plantshost.features.auth
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bootcampnttdata6.plantshost.R
-import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
 class AuthActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
-
     }
 }
--- app/src/main/java/com/bootcampnttdata6/plantshost/features/auth/sign_up/SignUpFragment.kt
+++ app/src/main/java/com/bootcampnttdata6/plantshost/features/auth/sign_up/SignUpFragment.kt
@@ -1,34 +1,32 @@
-package com.bootcampnttdata6.plantshost.features.auth.launch
+package com.bootcampnttdata6.plantshost.features.auth.sign_up
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
 import com.bootcampnttdata6.plantshost.R
 import com.bootcampnttdata6.plantshost.databinding.FragmentLaunchBinding
+import com.bootcampnttdata6.plantshost.databinding.FragmentSignUpBinding
+import com.bootcampnttdata6.plantshost.features.main.MainActivity
 
-class LaunchFragment : Fragment() {
-    private var _binding: FragmentLaunchBinding? = null
+class SignUpFragment : Fragment() {
+    private var _binding: FragmentSignUpBinding? = null
     private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
+    ): View? {
         // Inflate the layout for this fragment
-        _binding = FragmentLaunchBinding.inflate(inflater, container, false)
+        _binding = FragmentSignUpBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.btnGoToSignIn.setOnClickListener { findNavController().navigate(R.id.action_launch_to_sign_in) }
-        binding.btnGoToSignUp.setOnClickListener { findNavController().navigate(R.id.action_launch_to_sign_up) }
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+        binding.tvSignUp.setOnClickListener {
+            startActivity(Intent(requireActivity(),MainActivity::class.java))
+        }
     }
 }
