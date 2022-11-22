31cbab777f7acdf13f2e4de3ee8a284ea33fd39b
GM-DC <gm.flores.pariona@gmail.com>
2022-11-22 17:56:09 -0500

    Implement Favorite

---
+++ app/src/main/java/com/bootcampnttdata6/plantshost/features/main/favorite/presentation/FavoriteFragment.kt
@@ -0,0 +1,65 @@
+package com.bootcampnttdata6.plantshost.features.main.favorite.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bootcampnttdata6.plantshost.databinding.FragmentFavoriteBinding
+import com.bootcampnttdata6.plantshost.features.main.favorite.presentation.adapter.FavoriteAdapter
+import com.bootcampnttdata6.plantshost.features.main.home.domain.model.Plants
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class FavoriteFragment : Fragment() {
+
+    private var _binding:FragmentFavoriteBinding? = null
+    private val binding get() = _binding!!
+
+    lateinit var viewModel: FavoriteViewModel
+
+    private lateinit var favoriteAdapter: FavoriteAdapter
+    private val listaFavoritePlants = mutableListOf<Plants>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel =
+            activity?.let {
+                ViewModelProvider(it)[FavoriteViewModel::class.java]
+            }!!
+    }
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle? ): View {
+        _binding = FragmentFavoriteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initPlantsRecyclerview()
+        getListTable()
+    }
+
+    private fun getListTable() {
+        viewModel.listPlants.observe(viewLifecycleOwner){ it ->
+            favoriteAdapter.setItems(it)
+        }
+    }
+    private fun initPlantsRecyclerview() {
+        binding.rvFavorite.layoutManager = GridLayoutManager(activity,2, RecyclerView.VERTICAL,false)
+        favoriteAdapter = FavoriteAdapter(listaFavoritePlants) {
+                dataPlants -> onClickListener(dataPlants)
+        }
+        binding.rvFavorite.adapter = favoriteAdapter
+    }
+    private fun onClickListener(dataPlants: Plants) {
+        Toast.makeText(activity, "$dataPlants", Toast.LENGTH_SHORT).show()
+        val action = FavoriteFragmentDirections.actionFavoriteToDetail(dataPlants.id)
+        findNavController().navigate(action)
+    }
+
+}
