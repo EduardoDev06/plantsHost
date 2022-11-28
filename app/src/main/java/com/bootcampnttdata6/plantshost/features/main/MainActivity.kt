b40a754e90a0540cf6e6a2b3bb7dae4b852cc5bf
EduardoDev06 <wolf1200123@gmail.com>
2022-11-28 16:16:26 -0500

    Revert merge ivan_copia

--- app/src/main/java/com/bootcampnttdata6/plantshost/features/main/MainActivity.kt
+++ app/src/main/java/com/bootcampnttdata6/plantshost/features/main/MainActivity.kt
@@ -2,14 +2,9 @@ package com.bootcampnttdata6.plantshost.features.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
-import android.widget.Toast
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.bootcampnttdata6.plantshost.R
@@ -30,59 +25,7 @@ class MainActivity : AppCompatActivity() {
         navController = navHostFragment.navController
         appBarConfiguration = AppBarConfiguration(setOf(R.id.home, R.id.favorite, R.id.profile))
         val navView = binding.navView
-        navController.addOnDestinationChangedListener { _, destination, _ ->
-            when (destination.id) {
-                R.id.home -> navView.visibility = View.VISIBLE
-                R.id.favorite -> navView.visibility = View.VISIBLE
-                R.id.profile -> navView.visibility = View.VISIBLE
-                else -> navView.visibility = View.GONE
-            }
-        }
         navView.setupWithNavController(navController)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-    }
-
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.topbar_menu, menu)
-        navController.addOnDestinationChangedListener { _, destination, _ ->
-            when (destination.id) {
-                R.id.home -> {
-                    menu?.findItem(R.id.action_search)?.isVisible = true
-                    menu?.findItem(R.id.action_logout)?.isVisible = true
-                }
-                R.id.favorite -> {
-                    menu?.findItem(R.id.action_search)?.isVisible = false
-                    menu?.findItem(R.id.action_logout)?.isVisible = true
-                }
-                R.id.profile -> {
-                    menu?.findItem(R.id.action_search)?.isVisible = false
-                    menu?.findItem(R.id.action_logout)?.isVisible = true
-                }
-                else -> {
-                    menu?.findItem(R.id.action_search)?.isVisible = false
-                    menu?.findItem(R.id.action_logout)?.isVisible = false
-                    menu?.findItem(R.id.action_more)?.isVisible = false
-
-                }
-            }
-        }
-        return super.onCreateOptionsMenu(menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.action_logout -> {
-                Toast.makeText(this, "Regresa pronto", Toast.LENGTH_SHORT).show()
-                true
-            }
-            else -> return super.onOptionsItemSelected(item)
-        }
-
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+        setupActionBarWithNavController(navController,appBarConfiguration)
     }
 }
--- app/src/main/java/com/bootcampnttdata6/plantshost/features/main/home/presenter/view/HomeFragment.kt
+++ app/src/main/java/com/bootcampnttdata6/plantshost/features/main/home/presenter/view/HomeFragment.kt
@@ -1,20 +1,16 @@
 package com.bootcampnttdata6.plantshost.features.main.home.presenter.view
 
 import android.os.Bundle
-import android.view.*
 import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
-import androidx.appcompat.widget.SearchView
-import androidx.core.view.MenuHost
-import androidx.core.view.MenuProvider
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
-import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.StaggeredGridLayoutManager
-import com.bootcampnttdata6.plantshost.R
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bootcampnttdata6.plantshost.databinding.FragmentHomeBinding
 import com.bootcampnttdata6.plantshost.features.main.home.domain.model.Plants
 import com.bootcampnttdata6.plantshost.features.main.home.presenter.adapter.PlantsAdapter
@@ -23,12 +19,12 @@ import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), PlantsAdapter.OnClickListener {
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
-    private val plantsAdapter = PlantsAdapter(this::onClick)
-    private var searchView: SearchView? = null
+    private val plantsAdapter = PlantsAdapter(this)
     private val homeViewModel: HomeViewModel by viewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -41,57 +37,15 @@ class HomeFragment : Fragment() {
         homeViewModel.fetchListPlants()
         initRecycler()
         initViewModel()
-        listenerBtn()
-        hideScrollFloatingBtn()
-        menuItems()
-    }
-    private fun menuItems() {
-        val menuHost: MenuHost = requireActivity()
-        menuHost.addMenuProvider(object : MenuProvider {
-            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
-                //Ya se inflo el menu en el activity
-                val searchItem = menu.findItem(R.id.action_search)
-                searchView = searchItem.actionView as SearchView
-                searchView?.queryHint = "Buscar..."
-                searchView?.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-                    override fun onQueryTextSubmit(query: String): Boolean {
-                        searchView?.clearFocus()
-                        return true
-                    }
-
-                    override fun onQueryTextChange(newText: String): Boolean {
-                        plantsAdapter.filter.filter(newText)
-                        return true
-                    }
-                })
-            }
-            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
-                return true
-            }
-        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
-    }
-
-    private fun hideScrollFloatingBtn() {
-        binding.rvPlants.addOnScrollListener(object : RecyclerView.OnScrollListener() {
-            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-                if (dy < 0 && !binding.btnSave.isShown) {
-                    binding.btnSave.show()
-                } else if (dy > 0 && binding.btnSave.isShown) {
-                    binding.btnSave.hide()
-                }
-            }
-        })
-    }
-
-    private fun listenerBtn() {
-        binding.btnSave.setOnClickListener {
-            findNavController().navigate(R.id.action_home_to_createPlant)
-        }
     }
 
     private fun initRecycler() {
         binding.rvPlants.apply {
-            layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+            layoutManager = LinearLayoutManager(
+                context,
+                LinearLayoutManager.VERTICAL,
+                false
+            )
             adapter = plantsAdapter
         }
     }
@@ -101,17 +55,14 @@ class HomeFragment : Fragment() {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
                 homeViewModel.uiState.collect { state ->
                     when (state) {
-                        is HomeViewModel.State.Loading -> {
-                            handleLoading(true)
-                        }
                         is HomeViewModel.State.Success -> {
                             state.resultPlants.let { data ->
                                 if (data.isNotEmpty()) {
-                                    plantsAdapter.setData(data.toMutableList())
+                                    plantsAdapter.submitList(data)
                                 } else {
                                     Toast.makeText(
                                         requireContext(),
-                                        "No encontramos registros",
+                                        "Error desconocido",
                                         Toast.LENGTH_SHORT
                                     ).show()
                                 }
@@ -125,6 +76,9 @@ class HomeFragment : Fragment() {
                                     .show()
                             }
                         }
+                        HomeViewModel.State.Loading -> {
+                            handleLoading(true)
+                        }
                     }
                 }
             }
@@ -134,20 +88,22 @@ class HomeFragment : Fragment() {
     private fun handleLoading(isLoading: Boolean) {
         with(binding) {
             if (isLoading) {
-                viewLoading.visibility = View.VISIBLE
+                progress.visibility = View.VISIBLE
                 rvPlants.visibility = View.GONE
-                btnSave.visibility = View.GONE
             } else {
-                viewLoading.visibility = View.GONE
+                progress.visibility = View.GONE
                 rvPlants.visibility = View.VISIBLE
-                btnSave.visibility = View.VISIBLE
             }
         }
     }
 
-    private fun onClick(itemPlant: Plants) {
-        val action = HomeFragmentDirections.actionHomeToDetailFragment(itemPlant.id)
-        findNavController().navigate(action)
+    override fun onClick(resultPlants: Plants) {
+        Toast.makeText(
+            requireContext(),
+            "${resultPlants.id}, ${resultPlants.nameplant}, ${resultPlants.status}, ${resultPlants.price}, ${resultPlants.isFavorite}",
+            Toast.LENGTH_SHORT
+        ).show()
+        //findNavController().navigate(R.id.action_home_to_createPlant)
     }
 
     override fun onDestroyView() {
